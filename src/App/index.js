ff204830904a062f1490fd48707994a2fc61718a
quang0111 <50236477+quang0111@users.noreply.github.com>
2021-01-04 12:05:14 +0700

    hello

---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function HomePage() {
+  return (
+    <div className="home">
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default HomePage;
